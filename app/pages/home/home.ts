1a4de7b16483c6a8d037003b8761713bac51054e
ansodev <andersonfloriano@air-de-anderson.home>
2016-09-06 13:07:00 -0300

    Aula 08

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import { Messages } from '../../util/messages';
+import { Geolocation } from 'ionic-native';
+
+declare var geolib: any;
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -13,9 +16,34 @@ export class HomePage {
     this.initPage();
   }
 
-  initPage() {
+  private initPage() {
     this.messages.get(message => {
       this.messagesList.push(message);
     });
+
+    setInterval(() => {
+      this.getAllDistances();
+    }, 3000);
+  }
+
+  private getAllDistances() {
+    Geolocation.getCurrentPosition().then(resp => {
+      for (let i = 0; i < this.messagesList.length; i++) {
+        let message = this.messagesList[i];
+
+        message.distance = this.getDistance(
+          {latitude: resp.coords.latitude,
+          longitude: resp.coords.longitude},
+          {latitude: message.lat,
+          longitude: message.lng}
+        )
+      }
+    });
+  }
+
+  private getDistance(origin, destination) {
+    let distance = geolib.getDistance(origin, destination);
+
+    return geolib.convertUnit('km', distance, 2);
   }
 }
